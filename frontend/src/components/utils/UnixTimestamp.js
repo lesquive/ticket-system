d10210e17ea8c0a7257d17487470a1a2b4a375cb
lesquive <luis.jose.esquivel@gmail.com>
2023-02-07 19:37:48 -0600

    modififed time display to include hours/minutes/seconds

--- frontend/src/components/utils/UnixTimestamp.js
+++ frontend/src/components/utils/UnixTimestamp.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 export default function UnixTimestamp(props) {
-  const timestamp = props.timestamp;
-  const date = new Date(timestamp * 1000);
-  return date.toLocaleDateTimeString();
+  let timestamp = props.timestamp;
+  let date = new Date(timestamp * 1000);
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let hours = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+  let minutes =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  let seconds =
+    date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds();
+  return `${hours}:${minutes}:${seconds} ${year}-${month}-${day}`;
 }
